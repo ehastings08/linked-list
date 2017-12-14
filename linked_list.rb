0ee678239f3bd948f7ea5ce8848626092813b983
Ellie <elliehastings@gmail.com>
2017-12-13 19:50:48 -0500

    Add linked list printing

--- linked_list.rb
+++ linked_list.rb
@@ -1,18 +1,32 @@
 class LinkedList
+  def initialize(value)
+    @head = Node.new(value, nil)
+  end
+
+  # Adds a new node to the end of the list
+  def append
+    # Start with the head
+    # Keep nexting until you reach the element that has a pointer to nil
+    # Add new element with pointer to nil and add a pointer to that element to the old last element
+  end
+
+  def to_s
+    # Start with the head
+    current = @head
+    # Keep nexting and printing until done
+    puts current
+    until current.next_node == nil
+      current = current.next_node
+    end
+  end
 end
 
 class Node
-  def initialize(value=nil)
+  attr_accessor :value, :next_node
+
+  def initialize(value=nil, next_node=nil)
     @value = value
-    @next_node = nil
-  end
-
-  def value
-    @value
-  end
-
-  def next_node
-    @next_node
+    @next_node = next_node
   end
 
   def to_s
@@ -21,4 +35,10 @@ class Node
 end
 
 node1 = Node.new(1)
-puts node1
+#puts node1
+
+node2 = Node.new(2, 1)
+#puts node2
+
+linked_list = LinkedList.new(1)
+puts linked_list
